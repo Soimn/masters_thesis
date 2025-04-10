b67355c63dc18b60b312c9728ac03ae639429c89
Simon Doksrød <simon.doksrod@outlook.com>
2025-04-10 07:23:53 +0200

    reorganization

--- python_package/holocam/setup.py
+++ python_package/holocam/setup.py
@@ -4,4 +4,5 @@ import numpy
 setuptools.setup(
     ext_modules=[setuptools.Extension("holocam._c", ["module.c"], include_dirs=[numpy.get_include()])],
     install_requires=["numpy"],
+    data_files=[(".", ["holo_cam.dll"])],
 )
