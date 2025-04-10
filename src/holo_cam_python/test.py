47f52fd972d5ee04aab8bcd307e7448c21f685e2
Simon Doksrød <simon.doksrod@outlook.com>
2025-04-10 06:19:46 +0200

    opencv example

--- src/holo_cam_python/test.py
+++ src/holo_cam_python/test.py
@@ -1,6 +1,8 @@
 import holocam
 import numpy as np
 
+import cv2
+
 def main():
     width  = 1920
     height = 1080
@@ -8,6 +10,8 @@ def main():
     port   = 3003
 
     webcam = holocam.HoloCameraReader(holocam.get_camera_names()[0][1], width, height, fps)
+
+    face_model = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
     def camera_loop(args):
         effect = args.get("effect", 0)
@@ -20,6 +24,16 @@ def main():
             to_present[1::3] = 0
 
             return to_present
+        elif effect == 2:
+            frame = webcam.read_frame()
+
+            face_coords = face_model.detectMultiScale(frame)
+
+            if len(face_coords) == 0:
+                return frame
+            else:
+                x0, y0, w, h = face_coords[0]
+                return cv2.rectangle(frame, (x0, y0), (x0 + w, y0 + h), [0, 255, 0], 3)
         else:
             return webcam.read_frame()
 
